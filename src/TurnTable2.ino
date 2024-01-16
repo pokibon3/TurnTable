123317d57029fc67389ef175f2e1b5e7e79a9324
pokibon3 <pokibon@gmail.com>
2024-01-16 13:22:27 +0900

    add shake mode

--- src/TurnTable2.ino
+++ src/TurnTable2.ino
@@ -28,9 +28,11 @@
 #define GRAPH_HEIGHT  240
 #define GRAPH_WIDTH   320
 #define DISP_TIME      10
-#define FWD  1
-#define REW -1
-#define WAIT_TIME     300
+#define FWD             1
+#define REW            -1
+#define WAIT_TIME     200
+#define LIMIT_DEGREE   45
+
 #ifdef KAWAZU
     #define DEVICE_NAME   "TANTE2"  // for kawazu version
 #else
@@ -44,7 +46,18 @@ int16_t degree     = 0;
 int16_t oldAngle   = 0;
 int16_t clockWise  = FWD;
 int16_t motorSpeed = INIT_SPEED;
+int16_t angle      = 0;
+int16_t stepCount  = 0;         // number of steps the motor has taken
+
 bool    stopFlag   = false;
+
+enum {
+    FREERUN = 0,
+    SHAKERUN,
+};
+uint16_t runMode    = SHAKERUN;
+uint16_t oldRunMode = SHAKERUN;
+
 
 // instances
 Stepper myStepper(NBSTEPS, IN1, IN3, IN2, IN4);
@@ -56,7 +69,7 @@ void dispSpeed(int16_t speed)
 {
     char buf[64];
 
-    sprintf(buf, "Speed =%4d   ", speed);
+    sprintf(buf, "Speed = %4d   ", speed);
     Serial.println(buf);
 }
 
@@ -66,7 +79,7 @@ void dispSpeed(int16_t speed)
 void dispDegree(int16_t degree)
 {
     char buf[64];
-    sprintf(buf, "%04d", degree);
+    sprintf(buf, "degree= %4d", degree);
     Serial.println(buf);
 }
 
@@ -77,38 +90,51 @@ void btnCheck()
 {
     Dabble.processInput(); 
 
-    if (GamePad.isSelectPressed()) {
-        Serial.print("STOP");
-        stopFlag = true;
+    if (GamePad.isSelectPressed()) {        // "SELECT RUN MODE"
+        if (runMode == FREERUN) {
+            runMode = SHAKERUN;
+            oldRunMode = FREERUN;
+            Serial.println("SHAKERUN");
+        } else {
+            runMode = FREERUN;
+            oldRunMode = SHAKERUN;
+            Serial.println("FREERUN");
+        }
         delay(WAIT_TIME);
     }
-    if (GamePad.isStartPressed()){
-        Serial.println("START");
-        stopFlag = false;
+    if (GamePad.isStartPressed()){          // "START / STOP" 
+        if (stopFlag == true ) {
+            stopFlag = false;
+            Serial.println("START");
+        } else {
+            stopFlag = true;
+            Serial.println("STOP");
+        }
         delay(WAIT_TIME);
     }
-    if (GamePad.isSquarePressed()) {
+    if (GamePad.isSquarePressed()) {        // "□"
         Serial.println("Left");
         clockWise = REW;
         delay(WAIT_TIME);
     }
-    if (GamePad.isCirclePressed()) {
+    if (GamePad.isCirclePressed()) {        // "◯"
         Serial.println("Right");
         clockWise = FWD;
         delay(WAIT_TIME);
     }
-    if (GamePad.isTrianglePressed()) {
-        Serial.println("Triangle");
+    if (GamePad.isTrianglePressed()) {      // "△"
+        Serial.print("Triangle");
         motorSpeed++;
         if (motorSpeed > 15) motorSpeed = 15;
         myStepper.setSpeed(motorSpeed);
         if (motorSpeed > 1) {
             stopFlag = false;
         }
+        Serial.printf("\t: SpeedUp %3d\n", motorSpeed);
         delay(WAIT_TIME);
     }
-    if (GamePad.isCrossPressed()) {
-        Serial.println("Cross");
+    if (GamePad.isCrossPressed()) {         // "☓"
+        Serial.print("Cross");
         motorSpeed--;
         if (motorSpeed < 1) {
             motorSpeed = INIT_SPEED;
@@ -116,31 +142,9 @@ void btnCheck()
         }
         myStepper.setSpeed(motorSpeed);
         delay(WAIT_TIME);
+        Serial.printf("\t: SpeedUp %3d\n", motorSpeed);
     }
-    if (stopFlag) {
-        int angle = GamePad.getAngle();
-        if (angle <= 0) {
-            myStepper.setSpeed(motorSpeed);
-            clockWise = FWD;
-        } else {
-            angle = (360 - (angle - 90)) % 360;
-            if (angle > 180) angle -= 360;
-            if (angle != 0) {
-                if (angle > 0) {
-                    clockWise = FWD;
-                } else {
-                    clockWise = REW;
-                }
-                int wSpeed = abs(angle / 18);
-                if (wSpeed < 1) wSpeed = 1;
-                myStepper.setSpeed(wSpeed);
-                myStepper.step(clockWise);
-
-                Serial.print("Angle: ");
-                Serial.println(angle);
-            }
-        }
-    }
+    angle = GamePad.getAngle();
 }
 
 //========================================
@@ -150,21 +154,18 @@ void zeroCalibrate()
 {
     uint16_t 	voltage;
 
-   	pinMode(ADC_PIN, ANALOG);				// ADC in
-    analogSetAttenuation(ADC_11db);			// ADC ATT Level
-
     clockWise  = REW;
     for (int i = 0; i < NBSTEPS * 2; i++) {
         myStepper.step(clockWise);
         voltage = analogReadMilliVolts(ADC_PIN);
         //Serial.printf("voltage = %d mV\n", voltage);
         if (voltage >= ADC_ZERO) {
+            degree = 0;
             break;
         }
         delay(1);
     }
     clockWise  = FWD;
-    delay(3000);
 }
 
 //========================================
@@ -174,11 +175,15 @@ void setup(void)
 {
     Serial.begin(115200);
     Serial.println("Initilize...");
+   	pinMode(ADC_PIN, ANALOG);				// ADC in
+    analogSetAttenuation(ADC_11db);			// ADC ATT Level
+
    // set the motor speed:
     myStepper.setSpeed(abs(motorSpeed));
     dispSpeed(motorSpeed);
 #ifdef ZEROCAL
     zeroCalibrate();
+    delay(1000);
 #endif
     Dabble.begin(DEVICE_NAME);
     Serial.println("Starting...");
@@ -186,25 +191,110 @@ void setup(void)
 }
 
 //========================================
+// joyMode() : joystick control mode
+//========================================
+void joyMode()
+{
+    int16_t wClockWise;
+    int16_t wAngle;
+
+    if (angle <= 0) {
+        myStepper.setSpeed(motorSpeed);
+        clockWise = FWD;
+    } else {    
+        wAngle = (360 - (angle - 90)) % 360;
+        if (wAngle > 180) wAngle -= 360;
+        if (wAngle != 0) {
+            if (wAngle > 0) {
+                wClockWise = FWD;
+            } else {
+                wClockWise = REW;
+            }
+            int wSpeed = abs(wAngle / 18);
+            if (wSpeed < 1) wSpeed = 1;
+            myStepper.setSpeed(wSpeed);
+            myStepper.step(wClockWise);
+            stepCount += wClockWise;
+            Serial.print("wAngle: ");
+            Serial.println(wAngle);
+        }
+    }
+}
+
+//========================================
+// freeRunMode() : free Running
+//========================================
+void freeRunMode()
+{
+    myStepper.step(clockWise);
+    if (dispTime >= DISP_TIME) {
+        degree = (abs)((int)((stepCount) / 5.69) % 360);
+        //dispDegree(degree);
+        dispTime = 0;
+    } else {
+        dispTime++;
+    }
+    //Serial.print("steps:");
+    //Serial.println(stepCount);
+    stepCount += clockWise;
+}
+
+//========================================
+// shakeRunMode() : shake head Running
+//========================================
+void shakeRunMode()
+{
+    int16_t wSpeed;
+
+    if (oldRunMode != runMode) {
+        oldRunMode = runMode;
+#ifdef ZEROCAL
+        Serial.println("start ZeroCalibration");
+        zeroCalibrate();
+        delay(1000);
+#endif
+        degree = 0;
+        stepCount = 0;
+    }
+    degree = (int)((stepCount) / 5.69) % 360;
+    //dispDegree(degree);
+    wSpeed = motorSpeed;
+    if (degree >= 0) {
+        if (degree > LIMIT_DEGREE / 3 * 2) {
+            wSpeed = map(degree, LIMIT_DEGREE / 3 * 2, LIMIT_DEGREE, motorSpeed, 2);
+        }
+        if (clockWise == FWD && degree >= LIMIT_DEGREE) {
+            clockWise = REW;
+        }
+    } else {
+        if (degree <= -LIMIT_DEGREE / 3 * 2) {
+            wSpeed = map(degree, -LIMIT_DEGREE , -LIMIT_DEGREE / 3 * 2, 2, motorSpeed);
+        }
+        if (clockWise == REW && degree <= -LIMIT_DEGREE) {
+            clockWise = FWD;
+        }
+    }
+    myStepper.setSpeed(wSpeed);
+    //dispSpeed(wSpeed);
+    myStepper.step(clockWise);
+    stepCount += clockWise;
+}
+
+//========================================
 // loop : Arduino Main Loop
 //========================================
-int stepCount = 0;         // number of steps the motor has taken
 void loop()
 {
     btnCheck();
-    if (!stopFlag) {
-        myStepper.step(clockWise);
-        if (dispTime >= DISP_TIME) {
-            degree = (abs)((int)((stepCount) / 5.69) % 360);
-            //dispDegree(degree);
-            dispTime = 0;
-        } else {
-            dispTime++;
-        }
-        //Serial.print("steps:");
-        //Serial.println(stepCount);
-        stepCount += clockWise;
+    if (stopFlag) {
+        joyMode();
     } else {
-        delay(1);
+        if (runMode == FREERUN) {
+            freeRunMode();
+        } else {
+            shakeRunMode();
+        }
     }
+    delay(1);
+
 }
